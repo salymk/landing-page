8fba60a79fab1b79df2df1b52d4050e6c1b321c9
Salym Akhmedov <khokumdarakhmedov@gmail.com>
2020-01-08 11:58:07 -0700

    Selected section 1-3

--- js/app.js
+++ js/app.js
@@ -19,6 +19,9 @@
 */
 
 const navbarList = document.querySelector("#navbar__list");
+const section1 = document.querySelector("#section1");
+const section2 = document.querySelector("#section2");
+const section3 = document.querySelector("#section3");
 const li = document.createElement("li");
 
 
