3b5128265d3dac351d5707cb58b40a51451980ab
Salym Akhmedov <khokumdarakhmedov@gmail.com>
2020-01-11 11:43:00 -0700

    Created dynamic navbar

--- js/app.js
+++ js/app.js
@@ -19,10 +19,19 @@
 */
 
 const navbarList = document.querySelector("#navbar__list");
-const section1 = document.querySelector("#section1");
-const section2 = document.querySelector("#section2");
-const section3 = document.querySelector("#section3");
-const li = document.createElement("li");
+const sections = document.querySelectorAll("section");
+
+//Higher order functions
+sections.forEach((element) => {
+  const menuItem = element.dataset.nav;
+  const sectionId = element.id;
+  const li = document.createElement("li");
+  const a = document.createElement("a");
+  a.textContent = menuItem;
+  a.href = `#${sectionId}`;
+  li.appendChild(a);
+  navbarList.appendChild(li);
+});
 
 
 /**
@@ -31,44 +40,47 @@ const li = document.createElement("li");
  *
 */
 
-// target === where you want to scroll
-// duration === how long you want the scroll effect to take
-function smoothScroll(target, duration) {
-  var target = document.querySelector(target);
-  // returns coordinates relative to the viewport
-  var targetPosition = target.getBoundingClientRect().top;
-  // returns coordinates relative to the window
-  var startPosition = window.pageYOffset;
-  // returns distance
-  var distance = targetPosition - startPosition;
-  var startTime = null;
+// // target === where you want to scroll
+// // duration === how long you want the scroll effect to take
+// function smoothScroll(target, duration) {
+//   var target = document.querySelector(target);
+//   // returns coordinates relative to the viewport
+//   var targetPosition = target.getBoundingClientRect().top;
+//   // returns coordinates relative to the window
+//   var startPosition = window.pageYOffset;
+//   // returns distance
+//   var distance = targetPosition - startPosition;
+//   var startTime = null;
+//
+//   function animation(currentTime) {
+//     if(startTime === null) startTime = currentTime;
+//     var timeElapsed = currentTime - startTime;
+//     var run = ease(timeElapsed, startPosition, distance, duration);
+//     // Vertically scrolls
+//     window.scrollTo(0, run);
+//     // If timeElapsed is less than duration, call requestAnimationFrame.
+//     // This method tells the browser that you wish to perform an animation and requests
+//     // that the browser calls a specified function to update an animation before the next repaint
+//     if(timeElapsed < duration) requestAnimationFrame(animation);
+//   }
+//
+//   // Easing
+//     function ease (t, b, c, d) {
+//     t /= d/2;
+//     if (t < 1) return c/2*t*t*t + b;
+//     t -= 2;
+//     return c/2*(t*t*t + 2) + b;
+//   };
+//
+//   requestAnimationFrame(animation);
+// }
+//
+// const m1 = document.querySelector("#m1");
 
-  function animation(currentTime) {
-    if(startTime === null) startTime = currentTime;
-    var timeElapsed = currentTime - startTime;
-    var run = ease(timeElapsed, startPosition, distance, duration);
-    // Vertically scrolls
-    window.scrollTo(0, run);
-    // If timeElapsed is less than duration, call requestAnimationFrame.
-    // This method tells the browser that you wish to perform an animation and requests
-    // that the browser calls a specified function to update an animation before the next repaint
-    if(timeElapsed < duration) requestAnimationFrame(animation);
-  }
-
-  // Easing
-    function ease (t, b, c, d) {
-    t /= d/2;
-    if (t < 1) return c/2*t*t*t + b;
-    t -= 2;
-    return c/2*(t*t*t + 2) + b;
-  };
-
-  requestAnimationFrame(animation);
-}
-
-section1.addEventListener("click", function () {
-  smoothScroll(".section2", 1000);
-});
+// m1.addEventListener("click", function () {
+//   smoothScroll(".section2", 1000);
+//   console.log('click');
+// });
 
 
 /**
@@ -78,13 +90,13 @@ section1.addEventListener("click", function () {
 */
 
 // build the nav
-const menuItems = `
-  <li class="menu__items"><a href="#section1">Section 1</a></li>
-  <li class="menu__items"><a href="#section2">Section 2</a></li>
-  <li class="menu__items"><a href="#section3">Section 3</a></li>
-`;
-
-navbarList.innerHTML = menuItems;
+// const menuItems = `
+//   <li class="menu__items" id="m1" ><a href="#">Section 1</a></li>
+//   <li class="menu__items" id="m2"><a href="#">Section 2</a></li>
+//   <li class="menu__items" id="m3"><a href="#">Section 3</a></li>
+// `;
+//
+// navbarList.innerHTML = menuItems;
 
 
 // Add class 'active' to section when near top of viewport
