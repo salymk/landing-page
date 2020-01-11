5054d68335e5440da524eb3f682d083e8e13474e
Salym Akhmedov <khokumdarakhmedov@gmail.com>
2020-01-11 13:31:55 -0700

    Added smoothScroll function and called it on section1 and 2

--- js/app.js
+++ js/app.js
@@ -28,40 +28,54 @@ sections.forEach((element) => {
   a.href = `#${sectionId}`;
   li.appendChild(a);
   navbarList.appendChild(li);
+  a.classList = sectionId;
 });
 
 
-// // target === where you want to scroll
-// // duration === how long you want the scroll effect to take
-// function smoothScroll(target, duration) {
-//   var target = document.querySelector(target);
-//   // returns coordinates relative to the viewport
-//   var targetPosition = target.getBoundingClientRect().top;
-//   // returns coordinates relative to the window
-//   var startPosition = window.pageYOffset;
-//   // returns distance
-//   var distance = targetPosition - startPosition;
-//   var startTime = null;
-//
-//   function animation(currentTime) {
-//     if(startTime === null) startTime = currentTime;
-//     var timeElapsed = currentTime - startTime;
-//     var run = ease(timeElapsed, startPosition, distance, duration);
-//     // Vertically scrolls
-//     window.scrollTo(0, run);
-//     // If timeElapsed is less than duration, call requestAnimationFrame.
-//     // This method tells the browser that you wish to perform an animation and requests
-//     // that the browser calls a specified function to update an animation before the next repaint
-//     if(timeElapsed < duration) requestAnimationFrame(animation);
-//   }
-//
-//   // Easing
-//     function ease (t, b, c, d) {
-//     t /= d/2;
-//     if (t < 1) return c/2*t*t*t + b;
-//     t -= 2;
-//     return c/2*(t*t*t + 2) + b;
-//   };
-//
-//   requestAnimationFrame(animation);
-// }
+
+
+// target === where you want to scroll
+// duration === how long you want the scroll effect to take
+function smoothScroll(target, duration) {
+  var target = document.querySelector(target);
+  // returns coordinates relative to the viewport
+  var targetPosition = target.getBoundingClientRect().top;
+  // returns coordinates relative to the window
+  var startPosition = window.pageYOffset;
+  // returns distance
+  var distance = targetPosition - startPosition;
+  var startTime = null;
+
+  function animation(currentTime) {
+    if(startTime === null) startTime = currentTime;
+    var timeElapsed = currentTime - startTime;
+    var run = ease(timeElapsed, startPosition, distance, duration);
+    // Vertically scrolls
+    window.scrollTo(0, run);
+    // If timeElapsed is less than duration, call requestAnimationFrame.
+    // This method tells the browser that you wish to perform an animation and requests
+    // that the browser calls a specified function to update an animation before the next repaint
+    if(timeElapsed < duration) requestAnimationFrame(animation);
+  }
+
+  // Easing
+    function ease (t, b, c, d) {
+    t /= d/2;
+    if (t < 1) return c/2*t*t*t + b;
+    t -= 2;
+    return c/2*(t*t*t + 2) + b;
+  };
+
+  requestAnimationFrame(animation);
+}
+
+const section1 = document.querySelector("#section1");
+const section2 = document.querySelector("#section2");
+
+section1.addEventListener('click', function () {
+  smoothScroll(".section2", 1000);
+});
+
+section2.addEventListener('click', () => {
+  smoothScroll(".section1", 1000);
+});
